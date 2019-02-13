1623cee8bbeb68edb488dc73c90669c4ee012e08
Denys Sidorov <000scorpions000@gmail.com>
2019-02-13 22:18:53 +0200

    add shallow, mount, render kinds of tests

--- src/tests/components/List.test.js
+++ src/tests/components/List.test.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import {shallow} from '../enzyme';
+import {shallow, mount, render} from '../enzyme';
 
 import List from './List';
 
 describe('List tests', () => {
   it('renders list-items', () => {
     const items = ['one', 'two', 'three'];
-    const wrapper = shallow(<List items={items} />);
+
+    // Replace shallow iwth mount
+    const wrapper = mount(<List items={items} />);
+
+    // Let's check what wrong in our instance
+    console.log(wrapper.debug());
 
     // Expect the wrapper object to be defined
     expect(wrapper.find('.list-items')).toBeDefined();
     expect(wrapper.find('.item')).toHaveLength(items.length);
   });
+});
 
-  it('renders a list item', () => {
-    const items = ['Thor', 'Loki'];
-    const wrapper = shallow(<List items={items} />);
+it('renders list-items static', () => {
+  const items = ['one', 'two', 'three'];
+  const wrapper = render(<List items={items} />);
+  wrapper.addClass('foo');
+  // Expect the wrapper object to be defined
+  expect(wrapper.find('.list-items')).toBeDefined();
+  expect(wrapper.find('.item')).toHaveLength(items.length);
+});
 
-    // Check if an element in the Component exists
-    expect(
-      wrapper.contains(
-        <li key="Thor" className="item">
-          Thor
-        </li>,
-      ),
-    ).toBeTruthy();
-  });
-
-  it('renders correct text in item', () => {
-    const items = ['John', 'James', 'Luke'];
-    const wrapper = shallow(<List items={items} />);
-
-    // Expect the child of the first item to be an array
-    expect(wrapper.find('.item').get(0).props.children).toEqual('John');
-  });
+describe('List tests', () => {
+  // it('renders correct text in item', () => {
+  //   const items = ['John', 'James', 'Luke'];
+  //   const wrapper = shallow(<List items={items} />);
+  //
+  //   // Expect the child of the first item to be an array
+  //   expect(wrapper.find('.item').get(0).props.children).toEqual('John');
+  // });
 
   it('renders empty state', () => {
     const items = [];
